memmerto/FTL

commit e0609f14eee7903bca93020371576dad0ca93338

Make querytype string available everywhere in FTL.
Signed-off-by: DL6ER <dl6er@dl6er.de>

src/api/api.c
@@ -566,25 +566,22 @@ void getUpstreamDestinations(const char *client_message, const int *sock)
 	}
 }
 
-const char *querytypes[TYPE_MAX] = {"A", "AAAA", "ANY", "SRV", "SOA", "PTR", "TXT", "NAPTR",
-                                    "MX", "DS", "RRSIG", "DNSKEY", "OTHER", "UNKNOWN"};
-
 void getQueryTypes(const int *sock)
 {
 	int total = 0;
-	for(int i=0; i < TYPE_MAX-1; i++)
+	for(enum query_type type = TYPE_A; type < TYPE_MAX; type++)
 	{
-		total += counters->querytype[i];
+		total += counters->querytype[type];
 	}
 
-	float percentage[TYPE_MAX-1] = { 0.0 };
+	float percentage[TYPE_MAX] = { 0.0 };
 
 	// Prevent floating point exceptions by checking if the divisor is != 0
 	if(total > 0)
 	{
-		for(int i=0; i < TYPE_MAX-1; i++)
+		for(enum query_type type = TYPE_A; type < TYPE_MAX; type++)
 		{
-			percentage[i] = 1e2f*counters->querytype[i]/total;
+			percentage[type] = 1e2f*counters->querytype[type-1]/total;
 		}
 	}
 
@@ -593,38 +590,38 @@ void getQueryTypes(const int *sock)
 		             "SOA: %.2f\nPTR: %.2f\nTXT: %.2f\nNAPTR: %.2f\n"
 		             "MX: %.2f\nDS: %.2f\nRRSIG: %.2f\nDNSKEY: %.2f\n"
 		             "OTHER: %.2f\n",
-		      percentage[0], percentage[1], percentage[2], percentage[3],
-		      percentage[4], percentage[5], percentage[6], percentage[7],
-		      percentage[8], percentage[9], percentage[10], percentage[11],
-		      percentage[12]);
+		      percentage[TYPE_A], percentage[TYPE_AAAA], percentage[TYPE_ANY], percentage[TYPE_SRV],
+		      percentage[TYPE_SOA], percentage[TYPE_PTR], percentage[TYPE_TXT], percentage[TYPE_NAPTR],
+		      percentage[TYPE_MX], percentage[TYPE_DS], percentage[TYPE_RRSIG], percentage[TYPE_DNSKEY],
+		      percentage[TYPE_OTHER]);
 	}
 	else {
 		pack_str32(*sock, "A (IPv4)");
-		pack_float(*sock, percentage[0]);
+		pack_float(*sock, percentage[TYPE_A]);
 		pack_str32(*sock, "AAAA (IPv6)");
-		pack_float(*sock, percentage[1]);
+		pack_float(*sock, percentage[TYPE_AAAA]);
 		pack_str32(*sock, "ANY");
-		pack_float(*sock, percentage[2]);
+		pack_float(*sock, percentage[TYPE_ANY]);
 		pack_str32(*sock, "SRV");
-		pack_float(*sock, percentage[3]);
+		pack_float(*sock, percentage[TYPE_SRV]);
 		pack_str32(*sock, "SOA");
-		pack_float(*sock, percentage[4]);
+		pack_float(*sock, percentage[TYPE_SOA]);
 		pack_str32(*sock, "PTR");
-		pack_float(*sock, percentage[5]);
+		pack_float(*sock, percentage[TYPE_PTR]);
 		pack_str32(*sock, "TXT");
-		pack_float(*sock, percentage[6]);
+		pack_float(*sock, percentage[TYPE_TXT]);
 		pack_str32(*sock, "NAPTR");
-		pack_float(*sock, percentage[7]);
+		pack_float(*sock, percentage[TYPE_NAPTR]);
 		pack_str32(*sock, "MX");
-		pack_float(*sock, percentage[8]);
+		pack_float(*sock, percentage[TYPE_MX]);
 		pack_str32(*sock, "DS");
-		pack_float(*sock, percentage[9]);
+		pack_float(*sock, percentage[TYPE_DS]);
 		pack_str32(*sock, "RRSIG");
-		pack_float(*sock, percentage[10]);
+		pack_float(*sock, percentage[TYPE_RRSIG]);
 		pack_str32(*sock, "DNSKEY");
-		pack_float(*sock, percentage[11]);
+		pack_float(*sock, percentage[TYPE_DNSKEY]);
 		pack_str32(*sock, "OTHER");
-		pack_float(*sock, percentage[12]);
+		pack_float(*sock, percentage[TYPE_OTHER]);
 	}
 }
 
@@ -817,7 +814,7 @@ void getAllQueries(const char *client_message, const int *sock)
 		if(query->type > TYPE_MAX-1)
 			continue;
 		// Get query type
-		const char *qtype = querytypes[query->type - TYPE_A];
+		const char *qtype = querytypes[query->type];
 
 		// 1 = gravity.list, 4 = wildcard, 5 = black.list
 		if((query->status == QUERY_GRAVITY ||

src/datastructure.c
@@ -19,6 +19,9 @@
 // flush_message_table()
 #include "database/message-table.h"
 
+const char *querytypes[TYPE_MAX] = {"A", "AAAA", "ANY", "SRV", "SOA", "PTR", "TXT", "NAPTR",
+                                    "MX", "DS", "RRSIG", "DNSKEY", "OTHER", "UNKNOWN"};
+
 // converts upper to lower case, and leaves other characters unchanged
 void strtolower(char *str)
 {

src/datastructure.h
@@ -20,6 +20,8 @@ enum blocking_status { UNKNOWN_BLOCKED = 0u, GRAVITY_BLOCKED, BLACKLIST_BLOCKED,
 
 enum query_type { TYPE_A = 1, TYPE_AAAA, TYPE_ANY, TYPE_SRV, TYPE_SOA, TYPE_PTR, TYPE_TXT, TYPE_NAPTR,
        TYPE_MX, TYPE_DS, TYPE_RRSIG, TYPE_DNSKEY, TYPE_OTHER, TYPE_MAX } __attribute__ ((packed));
+const char *querytypes[TYPE_MAX];
+
 typedef struct {
 	unsigned char magic;
 	unsigned char status;

src/regex.c
@@ -74,29 +74,19 @@ static bool compile_regex(const char *regexin, const enum regex_type regexid)
 		// Analyze FTL-specific parts
 		while((part = strtok_r(NULL, FTL_REGEX_SEP, &saveptr)) != NULL)
 		{
-			char *extra;
-			if(sscanf(part, "querytype=%4ms", &extra))
+			char extra[5] = { 0 };
+			if(sscanf(part, "querytype=%4s", extra))
 			{
-				if(strcasecmp(extra, "A") == 0)
-					regex[index].query_type = TYPE_A;
-				else if(strcasecmp(extra, "AAAA") == 0)
-					regex[index].query_type = TYPE_AAAA;
-				else if(strcasecmp(extra, "ANY") == 0)
-					regex[index].query_type = TYPE_ANY;
-				else if(strcasecmp(extra, "SRV") == 0)
-					regex[index].query_type = TYPE_SRV;
-				else if(strcasecmp(extra, "SOA") == 0)
-					regex[index].query_type = TYPE_SOA;
-				else if(strcasecmp(extra, "PTR") == 0)
-					regex[index].query_type = TYPE_PTR;
-				else if(strcasecmp(extra, "TXT") == 0)
-					regex[index].query_type = TYPE_TXT;
-				else if(strcasecmp(extra, "NAPTR") == 0)
-					regex[index].query_type = TYPE_NAPTR;
-				else
+				for(enum query_type type = TYPE_A; type < TYPE_MAX; type++)
+				{
+					if(strcasecmp(extra, querytypes[type]) == 0)
+					{
+						regex[index].query_type = type;
+						break;
+					}
+				}
+				if(regex[index].query_type != 0)
 					logg("WARNING: Unknown query type: \"%s\"", extra);
-				free(extra);
-				extra = NULL;
 			}
 			else if(strcasecmp(part, "invert") == 0)
 			{

commit 07571df062e7406a1b3ae863973605e4b1028f73

Fix API computation error introduced in e0609f14eee7903bca93020371576dad0ca93338
Signed-off-by: DL6ER <dl6er@dl6er.de>

src/api/api.c
@@ -571,7 +571,7 @@ void getQueryTypes(const int *sock)
 	int total = 0;
 	for(enum query_type type = TYPE_A; type < TYPE_MAX; type++)
 	{
-		total += counters->querytype[type];
+		total += counters->querytype[type - 1];
 	}
 
 	float percentage[TYPE_MAX] = { 0.0 };
@@ -581,7 +581,7 @@ void getQueryTypes(const int *sock)
 	{
 		for(enum query_type type = TYPE_A; type < TYPE_MAX; type++)
 		{
-			percentage[type] = 1e2f*counters->querytype[type-1]/total;
+			percentage[type] = 1e2f*counters->querytype[type - 1]/total;
 		}
 	}
 